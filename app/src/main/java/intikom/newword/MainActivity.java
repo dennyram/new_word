a00855dc539e49a9e865578a4389bfca4ffd00da
Denny Ramadan <ramadan.denny@gmail.com>
2018-03-13 20:20:57 +0700

    - add push notification microsoft app center

--- app/src/main/java/intikom/newword/MainActivity.java
+++ app/src/main/java/intikom/newword/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
 import com.microsoft.appcenter.crashes.Crashes;
+import com.microsoft.appcenter.push.Push;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AppCenter.start(getApplication(), "9ff2b8a8-311d-4cd9-9f7b-4fd80c864b22", Analytics.class, Crashes.class);
+        AppCenter.start(getApplication(), "9ff2b8a8-311d-4cd9-9f7b-4fd80c864b22", Analytics.class, Crashes.class, Push.class);
     }
 }
